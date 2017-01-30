f386743b075d849775dde1dae695517ecb5864a3
Justin Jo <justinj7@gmail.com>
2017-01-30 08:08:17 -0500

    Added led struct elements

--- led.h
+++ led.h
@@ -3,7 +3,11 @@
 
 struct LED {
   int pin;
-  byte brightness;
+  byte lux; // brightness
+  byte maxLux; // for use in toggling
+  byte state; // 0 for blink, 1 for fade
+  int toggleHz;
+  int fadeSec;
   unsigned long startTime;
 };
 
