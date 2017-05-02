8f592cdd408b33f32b21014c8794bd2f2ccc3880
Justin Jo <justinj7@gmail.com>
2017-05-02 14:25:09 -0400

    Implemented led control

--- led.h
+++ led.h
@@ -1,1 +1,13 @@
+/* led.h
+ * Declaration of led control functions.
+ */
 
+#ifndef led_h
+#define led_h
+
+/********* Exported function declarations *********/
+extern void led_on(int pin);
+extern void led_off(int pin);
+extern void led_blink(int pin, int blinks);
+
+#endif
--- led.ino
+++ led.ino
@@ -1,1 +1,34 @@
+/* led.h
+ * Implementation of led control functions.
+ */
+
+ #include "led.h"
+
+/********* Constant declarations *********/
+static const int DELAY = 100;
+
+ /********* Exported function definitions *********/
+extern void led_on(int pin)
+{
+  digitalWrite(pin, HIGH);
+}
+
+extern void led_off(int pin)
+{
+  digitalWrite(pin, LOW);
+}
+
+extern void led_blink(int pin, int blinks)
+{
+  int blink_count = 0;
+  unsigned long start_time = millis();
+  
+  while (blink_count < blinks) {
+    led_on(pin);
+    while (millis() - start_time < DELAY) {}
+    led_off(pin);
+    while (millis() - start_time < DELAY * 2) {}
+    blink_count++;
+  }
+}
 
