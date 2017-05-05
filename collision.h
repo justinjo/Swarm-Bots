05a35a8a466624cd90c209625d96b8377bca38d4
Justin Jo <justinj7@gmail.com>
2017-05-05 10:35:13 -0400

    Implemented collision detection

--- collision.h
+++ collision.h
@@ -1,1 +1,16 @@
+/* collision.h
+ * Declaration of collision detection protocols.
+ */
 
+#ifndef collision_h
+#define collision_h
+
+/********* Typedef declarations *********/
+typedef enum Bumper {
+  fr_bump = 1, f_bump = 2, fl_bump = 4, r_bump = 8;
+} Bumper;
+
+/********* Exported function declarations *********/
+extern byte check_bumpers();
+
+#endif
--- collision.ino
+++ collision.ino
@@ -1,1 +1,22 @@
+/* collision.ino
+ * Implementation of collision detection protocols.
+ */
+
+#include "collision.h"
+
+/********* Exported function definitions *********/
+/* reads all the bumper pins and returns a byte denoting which bumpers
+ * have collided with the wall
+ */
+extern byte check_bumpers()
+{
+  byte collision = 0;
+  
+  collision |= (digitalRead(FR_BUMP)) ? fr_bump : collision;
+  collision |= (digitalRead(F_BUMP)) ? f_bump : collision;
+  collision |= (digitalRead(FL_BUMP)) ? fl_bump : collision;
+  collision |= (digitalRead(R_BUMP)) ? r_bump : collision;
+
+  return collision;
+}
 
