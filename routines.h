61db7f0cb824ddb546183b56e1156defff374e96
Justin Jo <justinj7@gmail.com>
2017-05-05 17:45:13 -0400

    Added routine stubs

--- routines.h
+++ routines.h
@@ -1,1 +1,13 @@
+/* routines.h
+ * Declaration of behavior protocols.
+ */
 
+#ifndef routines_h
+#define routines_h
+
+/********* Exported function declarations *********/
+extern void challenge1(int bot_num);
+extern void challenge2(int bot_num);
+extern void diagnostic();
+
+#endif
--- routines.ino
+++ routines.ino
@@ -1,1 +1,22 @@
+/* routines.ino
+ * Implementation of behavior protocols.
+ */
+
+#include "routines.h"
+
+/********* Exported function definitions *********/
+extern void challenge1(int bot_num)
+{
+  
+}
+
+extern void challenge2(int bot_num)
+{
+  
+}
+
+extern void diagnostic()
+{
+  
+}
 
